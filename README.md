878cd60d770040c44e9d80c58db3a2108243e046
somewheve <somewheve@gmail.com>
2020-05-17 22:25:13 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fast-pub
 Python高性能交易行情网关 支持websocket. 
+
+
+## 项目描述
+尝试使用Python fastapi + ctpbee来构建高性能行情/交易网关
+## EEEEEE   
+目标：
+200个账户
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='fast_pub',
+    version="0.1",
+    description="异步分发行情交易处理中心",
+    author='somewheve',
+    long_description="高性能行情网关",
+    author_email='somewheve@gmail.com',
+    url='https://github.com/ctpbee/fast-pub',
+    license="MIT",
+    packages=[],
+    install_requires=["ctpbee", "fastapi", "uvicorn"],
+    platforms=["Windows", "Linux", "Mac OS-X"],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
